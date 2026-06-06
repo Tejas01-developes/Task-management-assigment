462e7cec8c194c046d4290d94cd494cc59c4a723
Tejas01-developes <tlimbad7@gmail.com>
2026-06-06 22:22:46 +0530

    home page worjking

--- task-management/src/Pages/Home.tsx
+++ task-management/src/Pages/Home.tsx
@@ -1,17 +1,62 @@
-import  { useContext, useState } from 'react'
+import  { use, useContext, useEffect, useState } from 'react'
 import { Authcontext } from '../Tokenhandelling'
 import axios from 'axios'
+import { useNavigate } from 'react-router-dom'
 
 const Home = () => {
+    const navigate=useNavigate()
     const{getaccess}=useContext(Authcontext)
+    const{setaccess}=useContext(Authcontext)
     const token=getaccess()
 const[field,setfield]=useState({title:"",description:""})
+const[loading,setloading]=useState(true)
+const[tasks,settasks]=useState([])
 
 const handlefield=(e)=>{
     setfield({
         ...field,[e.target.name]:e.target.value
     })
 }
+useEffect(()=>{
+    const handlepage=async()=>{
+        // if(token){
+        //   return  setloading(false)
+        // }
+        try{
+            const res=await axios.post("http://localhost:3000/apis/refresh",{},{withCredentials:true})
+            if(res.data.success){
+                 setaccess(res.data.access) 
+                 return getalltsk(res.data.access)
+            }
+                alert("refresh filter failed")
+                return navigate("/")
+            
+        }catch(err){
+            alert("session loading failed")
+            return navigate("/")
+        }finally{
+            setloading(false)
+        }
+    }
+    handlepage()
+},[])
+
+
+const getalltsk=async(activetoken)=>{
+    if(!activetoken) return
+    try{
+    const res=await axios.get("http://localhost:3000/apis/fetch",{headers:{Authorization:`Bearer ${activetoken}`}}) 
+    if(res.data.success){
+        return settasks(res.data.result)
+    }
+    return alert(res.data.message)
+}catch(err){
+alert("task fetch failed")
+}
+}
+
+
+
 
 
 const addtask=async()=>{
@@ -36,12 +81,21 @@ if(res.data.success){
 
   return (
     <div>
-   <h1>{token}</h1>
+  {loading? <h1>Loading.....</h1> : token}
  
    <input type="text" placeholder='Title' name='title' value={field.title} onChange={handlefield}/>
 
    <textarea name="description" placeholder='Description' value={field.description} onChange={handlefield}/>
    <button onClick={addtask}>Add Task</button>
+
+{tasks.map((i,key)=>(
+<div key={key}>
+{i.title}
+{i.description}
+{i.status}
+</div>
+))}
+
     </div>
   )
 }
